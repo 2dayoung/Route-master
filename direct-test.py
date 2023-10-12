5ecdf2c9e9cede8f500b1655577c05497b705209
Lee-dayoung <daylee4564@naver.com>
2023-10-12 22:43:06 +0900

    모듈 이용해서 각각 함수 가져와서 좌표 변환해서 리스트에 저장하고 그걸로 거리, 소요시간 구하는데 list에 랜덤한 순서로 변환된 좌표가 들어가는 문제 발생

---
+++ direct-test.py
@@ -0,0 +1,36 @@
+import requests
+import get_taketime
+import get_latlon
+
+
+if __name__ == "__main__":
+    naver_api_key = "a986bwktfz"
+    secretcode= "uEQ7c34iw9s6XQNeIzcRungn858UJNL79g3ifFth"
+
+    address = "충청북도 청주시 서원구 모충로 32"  # 원하는 주소로 변경
+
+    address_list={"충청북도 청주시 서원구 사창동 367-7",
+                "충청북도 청주시 서원구 1순환로680번길 13-11",
+                "충청북도 청주시 서원구 모충로3번길 26",
+                "충청북도 청주시 서원구 내수동로 140",
+                "충청북도 청주시 서원구 창직로31번길 12-1"}
+    
+    # coordinates = get_latlon.address_to_coordinates(naver_api_key, secretcode, address) #좌표 
+    coordinate_list = []
+    for i, address in enumerate(address_list):
+        
+        coordinates = get_latlon.address_to_coordinates(naver_api_key, secretcode, address)
+        longitude, latitude = coordinates
+        coordinate_list.append(str(longitude)+','+str(latitude))
+
+    print(coordinate_list)
+    start_location=coordinate_list[0]
+    destination_location=coordinate_list[1]
+
+    distance, seconds = get_taketime.get_travel_time(naver_api_key,secretcode, start_location, destination_location)
+        # 초를 분과 초로 변환
+    minutes = seconds // 60  # 분
+    remaining_seconds = seconds % 60  # 초
+    print(start_location, destination_location)
+
+    print(f"출발지에서 목적지까지 {distance}m 거리이며, 약 {minutes}분  {remaining_seconds}초 소요됩니다.")
