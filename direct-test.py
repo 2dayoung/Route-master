c3838f294f093ca25f35f87b390c43725a04e801
Lee-dayoung <daylee4564@naver.com>
2023-12-18 01:47:04 +0900

    인접행렬 생성까지

--- direct-test.py
+++ direct-test.py
@@ -3,13 +3,43 @@ import get_taketime
 import get_latlon
 
 
+
+def make_adjlist(distance,_time,address_list) : 
+
+    # 인접행렬 생성
+    num_addresses = len(address_list) + 1
+    adjacency_matrix = [[-1] * num_addresses for _ in range(num_addresses)]
+    k=0
+    for i in range(len(address_list)):
+        for j in range(i, len(address_list)):
+            print(i,"  ",j)
+            if i != j:
+                adjacency_matrix[i+1][j+1] = distance[k]
+                adjacency_matrix[j+1][i+1] = distance[k]  # 무방향 그래프이므로 대칭
+                k+=1
+            else :
+                adjacency_matrix[i+1][j+1] = 0
+                
+
+    print("인접행렬:")
+    for row in adjacency_matrix:
+        print(row)
+
+# for address_pair, distance in distances.items():
+#     print(f'{address_pair}: {distance:.2f} km')
+
+# for address_pair, second in timee.items():
+#     print(f'{address_pair}:  약 {second[0]}분  {second[1]}초 소요됩니다.')
+
 if __name__ == "__main__":
     naver_api_key = "a986bwktfz"
     secretcode= "uEQ7c34iw9s6XQNeIzcRungn858UJNL79g3ifFth"
 
     # address = "충청북도 청주시 서원구 모충로 32"  # 원하는 주소로 변경
 
-    address_list=["충청북도 청주시 서원구 사창동 367-7",
+    address_list=[
+                "충청북도 청주시 흥덕구 사운로 375",
+                "충청북도 청주시 서원구 사창동 367-7",
                 "충청북도 청주시 서원구 1순환로680번길 13-11",
                 "충청북도 청주시 서원구 모충로3번길 26",
                 "충청북도 청주시 서원구 내수동로 140",
@@ -28,9 +58,9 @@ if __name__ == "__main__":
 
     addresses = list(dict_address.keys())
     coordinate = list(dict_address.values())
-    # print("addresses ",addresses )
-    # print("coordinate",coordinate)
 
+    print("coordinate",coordinate)
+    time_raw = []
     for i in range(len(coordinate)):
         for j in range(i + 1, len(coordinate)):
             start_location = coordinate[i]
@@ -38,7 +68,7 @@ if __name__ == "__main__":
             distance, seconds = get_taketime.get_travel_time(naver_api_key,secretcode, start_location, destination_location)
             address_pair=f'{addresses[i]} - {addresses[j]}'
             distances[address_pair] = distance
-
+            time_raw.append(seconds)
                 # 초를 분과 초로 변환
             minutes = str(int(seconds // 60) ) # 분
             remaining_seconds =str( int(seconds % 60))  # 초
@@ -46,10 +76,9 @@ if __name__ == "__main__":
             timee[address_pair] = [minutes,remaining_seconds]
 
     # 결과 출력
-for address_pair, distance in distances.items():
-    print(f'{address_pair}: {distance:.2f} km')
+    list_distance = list(distances.values())
+    list_time = time_raw
+    print(list_distance)
+    print(list_time)
 
-for address_pair, second in timee.items():
-    print(f'{address_pair}:  약 {second[0]}분  {second[1]}초 소요됩니다.')
-
-    # print(f"출발지에서 목적지까지 {distance}m 거리이며, 약 {minutes}분  {remaining_seconds}초 소요됩니다.")
+    make_adjlist(list_distance,list_time,address_list)
